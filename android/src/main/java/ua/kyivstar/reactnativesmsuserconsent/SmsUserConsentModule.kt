854621f0895ed0f1615ec96528bbe8cf54656674
DipeshChaudhari87 <dipesh.chaudhari@uniken.com>
2022-08-03 17:08:30 +0530

    Change the sender phone number in method startSmsUserConsent in UserConsentModule.kt file.

--- android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentModule.kt
+++ android/src/main/java/ua/kyivstar/reactnativesmsuserconsent/SmsUserConsentModule.kt
@@ -35,7 +35,7 @@ class SmsUserConsentModule(reactContext: ReactApplicationContext) : ReactContext
     }
     this.promise = promise
     if (reactContext?.currentActivity != null) {
-      val task: Task<Void> = SmsRetriever.getClient(reactContext.currentActivity!!).startSmsUserConsent("+917977509232")
+      val task: Task<Void> = SmsRetriever.getClient(reactContext.currentActivity!!).startSmsUserConsent("+919021977404")
 
       task.addOnSuccessListener { // successfully started an SMS Retriever for one SMS message
         registerReceiver()
